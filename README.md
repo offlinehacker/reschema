e5d1f19881e5dd307fc8d76e4580ad4ac45e3871
Jaka Hudoklin <jakahudoklin@gmail.com>
2016-04-07 22:08:51 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# reschema [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> Better schema
+
+## Installation
+
+```sh
+$ npm install --save reschema
+```
+
+## Usage
+
+```js
+var reschema = require('reschema');
+
+reschema('Rainbow');
+```
+## License
+
+MIT © [Jaka Hudoklin](https://gatehub.net)
+
+
+[npm-image]: https://badge.fury.io/js/reschema.svg
+[npm-url]: https://npmjs.org/package/reschema
+[travis-image]: https://travis-ci.org/offlinehacker/reschema.svg?branch=master
+[travis-url]: https://travis-ci.org/offlinehacker/reschema
+[daviddm-image]: https://david-dm.org/offlinehacker/reschema.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/offlinehacker/reschema
+[coveralls-image]: https://coveralls.io/repos/offlinehacker/reschema/badge.svg
+[coveralls-url]: https://coveralls.io/r/offlinehacker/reschema
---
+++ test/index.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+const Reschema = require('../lib');
+
+describe('reschema', function () {
+  it('should validate schema', () => {
+    const reschema = new Reschema({
+      type: 'sometype',
+      description: 'test',
+      properties: {
+        prop1: {
+          description: 'Prop 1',
+          extend: [
+            {type: 'typeone'},
+            {type: 'typetwo'}
+          ],
+          omit: ["prop2"],
+          properties: {
+            prop2: {
+              type: 'somethirdtype',
+              array: true
+            }
+          }
+        }
+      }
+    });
+  });
+
+  it('should convert to json schema', () => {
+    const loaderStub = sinon.stub().returns({
+      name: 'someothertype',
+      schema: {
+        properties: {
+          prop1: {validation: {type: 'string'}},
+          prop2: {validation: {type: 'integer'}}
+        }
+      }
+    });
+
+    const reschema = new Reschema({
+      description: 'some description',
+      array: true,
+      properties: {
+        prop1: {type: 'someothertype'}
+      }
+    }, {loader: loaderStub});
+
+    const context = {};
+    return reschema.toJSONSchema(context).then(jsonSchema => {
+      expect(jsonSchema).to.be.deep.equal({"type":"array","items":{"type":"object","properties":{"prop1":{"type":"object","allOf":["#/definitions/someothertype",{"type":"object","properties":{"prop1":{"type":"string"},"prop2":{"type":"integer"}}}]}}},"description":"some description"});
+      expect(context).to.be.deep.equal({"definitions":{"someothertype":{"type":"object","properties":{"prop1":{"type":"string"},"prop2":{"type":"integer"}}}}});
+    });
+  });
+
+  it('should resolve schema', () => {
+    const reschema = new Reschema({
+      extend: [{
+        description: 'some description',
+        properties: {
+          prop1: {description: 'prop1'},
+          prop2: {description: 'prop2'}
+        }
+      }, {
+        description: 'some other description',
+        properties: {
+          prop3: {description: 'prop3'},
+          prop1: {description: 'newprop1'}
+        }
+      }],
+      properties: {
+        prop4: {description: 'prop4'}
+      }
+    });
+
+    return reschema.expandProperties().then(properties => {
+      expect(properties).to.have.property('prop1');
+      expect(properties).to.have.property('prop2');
+      expect(properties).to.have.property('prop3');
+      expect(properties).to.have.property('prop4');
+      expect(properties.prop1.description).to.be.equal('newprop1');
+    });
+  });
+});
