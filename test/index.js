29bd365ca0fbf71a06fdd622ed2e7068ed578c6c
Jaka Hudoklin <jakahudoklin@gmail.com>
2016-04-24 16:14:42 +0200

    conversion to json schema, a few fixes

--- test/index.js
+++ test/index.js
@@ -51,16 +51,13 @@ describe('reschema', function () {
           schema: {validation: {type: 'integer'}}
         },
         prop3: {
-          schema: [
-            {
-              description: 'stringprop3',
-              validation: {type: 'string'}
-            },
-            {
-              description: 'intprop3',
-              extend: "type2"
-            }
-          ]
+          schema: [{
+            meta: {description: 'stringprop3'},
+            schema: {validation: {type: 'string'}},
+          }, {
+            meta: {description: 'intprop3'},
+            schema: "type2"
+          }]
         },
         prop5: 'type3',
         prop6: {schema: {items: 'type2'}}
@@ -79,11 +76,11 @@ describe('reschema', function () {
         .to.be.deep.equal({type: 'integer'});
       expect(schema.properties.prop1.meta.description).to.be.equal('prop1');
       expect(schema.properties.prop3.schema.kind).to.be.equal('alternatives');
-      expect(schema.properties.prop3.schema.alternatives[0].description)
+      expect(schema.properties.prop3.schema.alternatives[0].meta.description)
         .to.be.deep.equal('stringprop3');
-      expect(schema.properties.prop3.schema.alternatives[0].validation)
+      expect(schema.properties.prop3.schema.alternatives[0].schema.validation)
         .to.be.deep.equal({type: 'string'});
-      expect(schema.properties.prop3.schema.alternatives[1].validation)
+      expect(schema.properties.prop3.schema.alternatives[1].schema.validation)
         .to.be.deep.equal({type: 'integer'});
 
       expect(schema.properties.prop5.schema.kind).to.be.equal('type');
@@ -95,32 +92,113 @@ describe('reschema', function () {
     });
   });
 
-  it('should convert to json schema', () => {
-    const options = {
-      loader: sinon.stub().returns({
-        name: 'type2',
-        meta: {description: 'type2'},
-        schema: {
-          validation: {type: 'integer'}
+  describe('json schema', () => {
+    it('should convert simple value schema', () => {
+      return ReSchema.create(
+        {validation: {type: 'string'}}
+      ).then(schema => {
+        const jsonSchema = schema.toJSONSchema();
+        expect(jsonSchema.type).to.be.equal('string');
+      });
+    });
+
+    it('should convert object schema', () => {
+      return ReSchema.create({
+        properties: {
+          prop1: {
+            meta: {description: 'prop1'},
+            schema: {validation: {type: 'string'}}
+          },
+          prop2: {
+            meta: {description: 'prop2'},
+            schema: {
+              properties: {
+                subprop1: {
+                  meta: {description: 'subprop1'},
+                  schema: {validation: {type: 'integer'}}
+                }
+              }
+            }
+          }
         }
-      })
-    };
+      }).then(schema => {
+        const context = {};
+        const jsonSchema = schema.toJSONSchema(context);
 
-    const context = {};
-    return ReSchema.create({
-      properties: {
-        prop1: 'type2'
-      }
-    }, options).then(schema => {
-      const jsonSchema = schema.toJSONSchema(context);
+        expect(jsonSchema).to.be.deep.equal({
+          type: 'object',
+          properties: {
+            prop1: {type: 'string', description: 'prop1'},
+            prop2: {
+              type: 'object',
+              description: 'prop2',
+              properties: {
+                subprop1: {
+                  type: 'integer', description:'subprop1'
+                }
+              }
+            }
+          }
+        })
+      });
+    });
 
-      expect(jsonSchema).to.be.deep.equal({
-        type: 'object',
-        properties: {prop1: { '$ref': '#!/definitions/type2' }}
-      })
+    it('should convert referencing schema', () => {
+      const options = {
+        loader: sinon.stub().returns({
+          name: 'type2',
+          meta: {description: 'type2'},
+          schema: {
+            validation: {type: 'integer'}
+          }
+        })
+      };
 
-      expect(context).to.be.deep.equal({
-        definitions: {type2: { type: 'integer', description: 'type2' }}
+      const context = {};
+      return ReSchema.create({
+        properties: {
+          prop1: 'type2'
+        }
+      }, options).then(schema => {
+        const jsonSchema = schema.toJSONSchema(context);
+
+        expect(jsonSchema).to.be.deep.equal({
+          type: 'object',
+          properties: {
+            prop1: {
+              $ref: '#!/definitions/type2',
+              description: 'type2'
+            }
+          }
+        });
+
+        expect(context).to.be.deep.equal({
+          definitions: {type2: {type: 'integer', description: 'type2'}}
+        });
+      });
+    });
+
+    it('should convert array schema', () => {
+      return ReSchema.create({
+        items: {validation: {type: 'string'}}
+      }).then(schema => {
+        const jsonSchema = schema.toJSONSchema();
+        expect(jsonSchema).to.be.deep.equal({
+          type: 'array',
+          items: {type: 'string'}
+        });
+      });
+    });
+
+    it('should convert alternatives schema', () => {
+      return ReSchema.create([
+        {validation: {type: 'string'}},
+        {validation: {type: 'integer'}}
+      ]).then(schema => {
+        const jsonSchema = schema.toJSONSchema();
+        expect(jsonSchema).to.be.deep.equal({
+          anyOf: [{type: 'string'}, {type: 'integer'}]
+        });
       });
     });
   });
